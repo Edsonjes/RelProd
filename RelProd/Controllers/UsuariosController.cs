26ecdc95836182e5bdb8f635f2d5fa4f9970d825
Edsonjes <edsonjes@gmail.com>
2019-09-18 17:51:52 -0300

    apanhando para session

---
+++ RelProd/Controllers/UsuariosController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RelProd.Models;
+
+
+
+
+
+namespace RelProd.Controllers
+{
+	public class UsuariosController : Controller
+	{
+		RelProdContext _Ctx;
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Login( Usuarios Usuario)
+		{
+			
+			var consultaUsuario = _Ctx.Usuarios.Where(u => u.Email == Usuario.Email && u.Senha == Usuario.Senha).FirstOrDefault();
+
+			if ( consultaUsuario != null)
+			{
+
+				Session["UsuarioLogado"] = consultaUsuario.Email;
+				
+
+
+
+			}
+			else
+			{
+				ViewBag.ErroAutenticacao = "Usuário ou senha invalidos.";
+			}
+
+			return View();
+		}
+	}
+}
